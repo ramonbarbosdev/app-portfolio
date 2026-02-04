9eaedf857e14ee9712205cfbbfff3187682c803e
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2026-02-04 15:59:58 -0300

    feat: sessao

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { Home } from "../../features/home/home";
 import { About } from "../../features/about/about";
 import { Projects } from "../../features/projects/projects";
+import { Stack } from "../../features/stack/stack";
+import { Experience } from "../../features/experience/experience";
+import { Contact } from "../../features/contact/contact";
 
 @Component({
   selector: 'app-layout',
-  imports: [Home, About, Projects],
+  imports: [Home, About, Projects, Stack, Experience, Contact],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
