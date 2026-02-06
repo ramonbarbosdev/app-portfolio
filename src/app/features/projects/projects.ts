313ccc83889aefbbb5db547b2c76060075a27a97
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2026-02-06 11:33:09 -0300

    feat: modificacao capa card

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -16,6 +16,7 @@ export interface Project {
 
   githubUrl?: string;
   liveUrl?: string;
+  date?: string;
 }
 
 
@@ -30,59 +31,114 @@ export class Projects {
 
     {
       id: '1',
-
       title: 'Sistema de Gestão Pública',
-
-      description: 'Plataforma SaaS para criação de orçamentos',
-
+      description: '',
       longDescription: `
-        Sistema completo de orçamentos com arquitetura escalável,
-        backend em Spring Boot e frontend em Angular.
-
-        Inclui:
-        - precificação dinâmica
-        - multi-tenant
-        - APIs REST robustas
-        - arquitetura modular
+        Desenvolvimento de um sistema completo para prefeituras, abrangendo módulos 
+      de administração, finanças, RH, Almoxarifado, Patrimônio, Contabilidade, 
+      Relatórios, entre outros. Projeto realizado em equipe sob contrato de confidencialidade. 
       `,
-
       coverImage: '/assets/projects/orcamento.png',
-
       technologies: [
-        'angular',
-        'spring',
+        'php',
         'postgresql',
-        'docker'
+        'linux'
       ],
-
       githubUrl: 'https://github.com/ramonbarbosdev',
-
-      liveUrl: 'https://ramoncode.com.br'
+      liveUrl: 'https://ramoncode.com.br',
+      date: ''
     },
 
     {
       id: '2',
-
-      title: 'Portal Transparência',
-
-      description: 'Portal público de dados governamentais',
-
+      title: 'Crédito Rural e Financeiro',
+      description: '',
       longDescription: `
-        Portal com sincronização entre PostgreSQL e MongoDB,
-        usando cronjobs e arquitetura desacoplada.
+        Desenvolvimento de sistema para crédito rural, consórcios e crédito pessoal, 
+        atuando como Desenvolvedor Full Stack com Spring Boot, Angular. Responsável 
+        também pela hospedagem em VPS, com banco de dados separado da aplicação 
+        e storage dedicado para anexos, garantindo organização, segurança e 
+        escalabilidade, além da criação e manutenção de relatórios gerenciais e 
+        operacionais utilizando JasperSoft. 
       `,
-
       coverImage: '/assets/projects/portal.png',
-
       technologies: [
-        'React',
-        'NestJS',
-        'MongoDB'
+        'spring',
+        'angular',
+        'java',
+        'docker',
+        'postgresql',
+
       ],
 
       githubUrl: 'https://github.com/ramonbarbosdev'
 
-    }
+    },
+    {
+      id: '3',
+      title: 'SyncDb',
+      description: '',
+      longDescription: `
+      Projeto autoral para sincronizar dados entre bancos locais e na nuvem. Devido à 
+      sua utilidade, foi estendido para uma versão desktop e adotado internamente 
+      pela empresa para auxiliar o ambiente de testes.
+      `,
+      coverImage: '/assets/projects/portal.png',
+      technologies: [
+        'spring',
+        'angular',
+        'mongodb',
+
+      ],
+      githubUrl: 'https://github.com/ramonbarbosdev'
+    },
+    {
+      id: '4',
+      title: 'Sistema de Business Intelligence',
+      description: '',
+      longDescription: `
+      Desenvolvimento de dashboards interativos para análise e visualização de 
+      dados públicos. Solução moderna e responsiva, pensada para facilitar a leitura 
+      de informações por gestores municipais. Projeto realizado em equipe sob 
+      contrato de confidencialidade.
+      `,
+      coverImage: '/assets/projects/portal.png',
+      technologies: [
+        'react',
+        'nestjs',
+        'mongodb',
+      ],
+      githubUrl: 'https://github.com/ramonbarbosdev'
+    },
+    {
+      id: '5',
+      title: 'Sistema de Orçamentos SaaS',
+      description: '',
+      longDescription: `
+      Sistema SaaS completo para criação, simulação e gestão de orçamentos com arquitetura moderna e escalável.
+      A plataforma permite configurar regras de precificação dinâmicas, definir campos personalizados e calcular valores automaticamente com base em diferentes estratégias de precificação.
+      O backend foi desenvolvido em Java com Spring Boot, seguindo princípios de arquitetura limpa e APIs REST robustas, enquanto o frontend foi construído em Angular com foco em experiência do usuário e reatividade.
+      O sistema suporta arquitetura multi-tenant, garantindo isolamento e flexibilidade para múltiplos clientes, além de ter sido projetado para alta escalabilidade, manutenibilidade e evolução contínua.
+      Principais características:
+      • precificação dinâmica baseada em regras  
+      • arquitetura multi-tenant  
+      • APIs REST escaláveis  
+      • frontend moderno e responsivo  
+      • arquitetura modular e extensível  
+      `,
+      coverImage: '/assets/projects/portal.png',
+      technologies: [
+        'spring',
+        'angular',
+        'postgresql',
+        'docker',
+        'typescript'
+      ],
+      githubUrl: 'https://github.com/ramonbarbosdev'
+
+    },
+
+
 
   ];
 
