710ac1ef2cbbb53f3527ccb79d8bf56011931183
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2026-02-06 17:15:11 -0300

    rota padrao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,9 @@ import { Routes } from '@angular/router';
 import { Layout } from './pages/layout/layout';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', component: Layout },
+    {
+    path: '',
+    component: Layout
+  }
 
 ];
